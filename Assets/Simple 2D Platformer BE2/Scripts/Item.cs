9cec30350269ed64374b24796287818df731915b
kwd421 <tonistark@naver.com>
2024-04-03 18:48:47 +0900

    github test

--- Assets/Simple 2D Platformer BE2/Scripts/Item.cs
+++ Assets/Simple 2D Platformer BE2/Scripts/Item.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("GitHubTest");
     }
 
     // Update is called once per frame
